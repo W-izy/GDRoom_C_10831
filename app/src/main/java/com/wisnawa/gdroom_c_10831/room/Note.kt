0db1fef83145c6f132e33d7bb5b1eecfab96f00c
wisnawa <putuwisnawaandrian@yahoo.co.id>
2022-09-21 23:49:08 +0700

    Initial commit

---
+++ app/src/main/java/com/wisnawa/gdroom_c_10831/room/Note.kt
@@ -0,0 +1,10 @@
+package com.wisnawa.gdroom_c_10831.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+@Entity
+data class Note (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val note: String )
